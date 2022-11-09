5d7e89a1cae1f2bade87032b2cdb241c9bedd320
Mukesh Mahato <mukeshmahato843@gmail.com>
2022-11-09 08:10:11 +0545

    Optimizing

--- circle.js
+++ circle.js
@@ -1,8 +1,8 @@
 
 let number = document.querySelector("#number");
 let count = 0;
-let percentVal = number.innerHTML.slice(0, number.innerHTML.length - 1);    // getting number after removing % i.e 65% as 65
-let interval = 2000 / percentVal;
+let percentVal = number.innerHTML.slice(0, -1);    // getting number after removing % i.e 65% as 65
+let interval = 2000 / percentVal;   // since our animation time is 2s so, interval = 2000 / percentVal
 setInterval(() => {
     if (count == percentVal) {
         clearInterval()
